85f14aa2933178449dcac1fa3e357fa55f6f8ba2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-10-19 23:19:27 +0100

    scafolded database via entity

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -7,15 +7,36 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+  @Id
+  @SequenceGenerator(
+          name = "student_sequence",
+          sequenceName = "student_sequence",
+          allocationSize = 1
+  )
+  @GeneratedValue(
+          generator = "student_sequence",
+          strategy = GenerationType.SEQUENCE)
   private Long id;
   private String name;
   private String email;
+
+  public Student(String name, String email, Gender gender) {
+    this.name = name;
+    this.email = email;
+    this.gender = gender;
+  }
+
+  @Enumerated(EnumType.STRING)
   private Gender gender;
 }
