396a0285afa53be273a2379419c4136491d42d31
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-10-20 00:09:47 +0100

    testing backend changes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -84,7 +84,7 @@ function App() {
         bordered
         title={() => "Students"}
         pagination={{ pageSize: 50 }}
-        scroll={{ y: 240 }}
+        scroll={{ y: 500 }}
       />
     );
   };
